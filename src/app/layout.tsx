7b0f1d76dad30fb9171f46a86e2a33a023b545ea
Lykos-79 <kumarabhisekh0111@gmail.com>
2026-01-14 13:50:31 +0530

    added chat box

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import AuthProvider from "@/components/AuthProvider";
 import CartProvider from "@/components/cart/CartProvider";
 import Footer from "@/components/Footer";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Akhand Bhakti",
@@ -27,6 +28,23 @@ export default function RootLayout({
             </header>
             {children}
             <Toaster richColors position="bottom-right" />
+            <Script
+              id="tawk-to"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/696750d0521ecd197e490559/1jetp7c3d';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+      })();
+    `,
+              }}
+            />
             <Footer />
           </CartProvider>
         </AuthProvider>
