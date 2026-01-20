783c674f4c5427be0221d31378e91e2959df2521
Lykos-79 <kumarabhisekh0111@gmail.com>
2026-01-20 13:58:20 +0530

    sa

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -97,7 +97,7 @@ export default function Banner() {
               src="/home/guru.JPG"
               alt="Guru"
               fill
-              className="object-cover"
+              className="object-center"
             />
           </div>
 
